b219481c80cb1ec4ad6a58eaf2ed60e619d70f1f
Pramod Jacob <psjacob68@gmail.com>
2020-03-25 12:47:37 -0400

    Add tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import './layout.css'
+
 const Layout = ({ children }) => (
-    <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+  <div className="mx-auto max-w-screen-md">
+    <main>{children}</main>
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  </div>
 )
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({ data }) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div>
       {JSON.stringify(data.allTpLocation.nodes)}
     </div>
   </Layout>
