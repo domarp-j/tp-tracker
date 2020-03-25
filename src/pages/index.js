49e4c148e1e5cb5b31aa76a965e015a97da71f84
Pramod Jacob <psjacob68@gmail.com>
2020-03-25 11:49:43 -0400

    Display raw tp data on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const query = graphql`
+  query TPLocations {
+    allTpLocation {
+      nodes {
+        address
+        available
+        id
+        store
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      {JSON.stringify(data.allTpLocation.nodes)}
     </div>
   </Layout>
 )
