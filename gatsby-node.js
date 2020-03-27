dccb3e805f89ef1c72e2e9a4e668b016d8878102
Pramod Jacob <psjacob68@gmail.com>
2020-03-27 17:11:30 -0400

    Save geocodes to local JSON on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
+/*****************************************************************/
+// Exports
+/*****************************************************************/
+
+const fs = require("fs");
+
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
   if (stage === "build-html") {
     actions.setWebpackConfig({
@@ -11,4 +17,47 @@ exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
       }),
     });
   }
+};
+
+exports.onPostBuild = async () => {
+  const firebase = require("firebase/app");
+
+  require("firebase/firestore");
+
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+
+  firebase.initializeApp({
+    apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+    authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  });
+
+  const snapshot = await firebase
+    .firestore()
+    .collection("coordinates")
+    .get();
+
+  let docData;
+
+  fs.writeFile(
+    "src/data/geocode.json",
+    JSON.stringify(
+      snapshot.docs.reduce((accum, doc) => {
+        docData = doc.data();
+        accum[docData.address] = {
+          lat: docData.lat,
+          lng: docData.lng,
+        };
+        return accum;
+      }, {}) || {}
+    ),
+    err => {
+      console.error(
+        "Something went wrong while trying to create geocode JSON: ",
+        err
+      );
+    }
+  );
 };
--- src/utils/geocoding.js
+++ src/utils/geocoding.js
@@ -1,62 +1,136 @@
 import firestore from "../utils/firebase";
 import axios from "axios";
 
-export default async address => {
+/**
+ * This file contains the address-to-coordinates logic for TP Tracker. It uses
+ * the Google Maps Geocoding API to fetch coordinates, but only as a last resort.
+ *
+ * The following steps are taken to retrieve the coordinates for a given address:
+ * - Check a static JSON file that is created at every build and updates with
+ *   the latest address coordinates stored in a Firestore database
+ * - Check the user's local storage
+ * - Check the Firestore database directly for the coordinates
+ * - As a last resort, use the expensive Google Maps Geocoding API to get the
+ *   coordinates, and save the coordinates to Firebase and local storage for
+ *   later use.
+ *
+ * Why local storage?
+ * - By saving the coordinates to local storage and checking it first, the client
+ *   does not have to repeatedly reach out to any external services to get
+ *   coordinate data.
+ *
+ * Why Firestore?
+ * - Firestore has much more forgiving rate limits than Google Maps. The
+ *   occasional fetch from the Firestore database is much cheaper than a Geocoding
+ *   API query.
+ */
+
+/*****************************************************************/
+// Geocode Data
+/*****************************************************************/
+
+let geocodeData;
+try {
+  geocodeData = require("../data/geocode.json");
+} catch (_) {}
+
+/*****************************************************************/
+// Helper Functions
+/*****************************************************************/
+
+const checkGeocodeJson = address => {
+  if (!geocodeData) return;
+
+  const coordData = geocodeData[address];
+
+  if (coordData) {
+    return {
+      lat: coordData.lat,
+      lng: coordData.lng,
+    };
+  }
+};
+
+const checkLocalStorage = address => {
+  const coords = localStorage.getItem(`tp tracker ${address}`);
+  if (coords) return JSON.parse(coords);
+};
+
+const saveToLocalStorage = (address, coords) => {
+  localStorage.setItem(`tp tracker ${address}`, JSON.stringify(coords));
+};
+
+const checkFirebase = async address => {
   try {
-    console.log("GC1-ls...");
-
-    // Check local storage first for coordinates
-    let coords = localStorage.getItem(`tp tracker ${address}`);
-    if (coords) return JSON.parse(coords);
-
-    console.warn("GC1-ls failed. GC2-fs...");
-
-    // Check firestore
-    coords = await firestore
+    const coords = await firestore
       .collection("coordinates")
       .doc(address)
       .get();
 
-    // If coordinates exist in firestore, then...
-    if (coords.data()) {
-      // Save to local storage
-      localStorage.setItem(
-        `tp tracker ${address}`,
-        JSON.stringify(coords.data())
-      );
-      return coords.data();
-    }
+    const coordData = coords.data();
 
-    console.warn("GC2-fs failed. GC3-gm...");
-
-    // Since address coordinates aren't stored anywhere, call Geocode API
-    const geocodeRes = await axios.get(
-      "https://maps.googleapis.com/maps/api/geocode/json",
-      {
-        params: {
-          address,
-          key: process.env.GATSBY_GMAPS_KEY,
-        },
-      }
-    );
-
-    if (geocodeRes.data.error_message) {
-      throw new Error("API ERROR");
-    } else {
-      coords = geocodeRes.data.results[0].geometry.location;
-
-      // Store coordinates in firestore
-      firestore
-        .collection("coordinates")
-        .doc(address)
-        .set(coords);
-
-      // Store coordinates in local storage
-      localStorage.setItem(`tp tracker ${address}`, JSON.stringify(coords));
-
-      return coords;
+    if (coordData) {
+      return {
+        lat: coordData.lat,
+        lng: coordData.lng,
+      };
     }
   } catch (error) {
-    console.error(error);
+    // TODO: Error handling
+    throw error;
+  }
+};
+
+const saveToFirebase = (address, coords) => {
+  firestore
+    .collection("coordinates")
+    .doc(address)
+    .set({ ...coords, address });
+};
+
+const addressGeocode = async address => {
+  const res = await axios.get(
+    "https://maps.googleapis.com/maps/api/geocode/json",
+    {
+      params: {
+        address,
+        key: process.env.GATSBY_GMAPS_KEY,
+      },
+    }
+  );
+
+  if (res.data.error_message) return;
+
+  return res.data.results[0].geometry.location;
+};
+
+/*****************************************************************/
+// Main
+/*****************************************************************/
+
+export default async address => {
+  let coords;
+
+  // console.log("GC1-js...");
+  coords = checkGeocodeJson(address);
+  if (coords) return coords;
+
+  // console.log("GC1-js failed. GC2-ls...");
+  coords = checkLocalStorage(address);
+  if (coords) return coords;
+
+  // console.log("GC2-ls failed. GC3-fs...");
+  coords = await checkFirebase(address);
+  if (coords) {
+    saveToLocalStorage(address, coords);
+    return coords;
+  }
+
+  // console.log("GC3-fs failed. GC4-gm...");
+  coords = await addressGeocode(address);
+  if (coords) {
+    saveToLocalStorage(address, coords);
+    saveToFirebase(address, coords);
+    return coords;
   }
 };
